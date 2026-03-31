60be8eb1b344729cda90ed275210fa4ea0f2fa18
waimea-tdvu <tdvu@waimea.school.nz>
2026-03-31 14:22:22 +1300

    Get player name and symbol update

--- src/Main.kt
+++ src/Main.kt
@@ -27,24 +27,33 @@ fun main() {
         println("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛".cyan())
         println()
 
-        getPlayerName()
+        getPlayer1Name()
+        getPlayer2Name()
         createBoard()
-        showBoard()
-        getUser1Movement()
 
+        while (true) {
+            showBoard()
+            getUser1Movement()
+
+            showBoard()
+            getUser2Movement()
+        }
 
     }
     var player1Symbol = ""
     var player2Symbol = ""
 // Symbol P1 ♔♕⚐♡♢♤♧
 // Symbol P2 ♚♛⚑♥♦♠♣
-fun getPlayerName(){
+fun getPlayer1Name() {
     // Get user 1 name
 
     print(" Enter player 1 name here : ")
     val name1 = readln()
+    var choice1: Int?
+    val symbol1: String
 
-    // Get user 1 symbol
+    while (true) {
+        // Get user 1 symbol
         println("1.♔")
         println("2.♕")
         println("3.⚐")
@@ -53,26 +62,36 @@ fun getPlayerName(){
         println("6.♤")
         println("7.♧")
         println("Choose player 1 symbol to play: ")
-
-    var symbol1 = readln()
-        when {
-            symbol1 == "1"-> symbol1 = "♔"
-            symbol1 == "2"-> symbol1 = "♕"
-            symbol1 == "3"-> symbol1 = "⚐"
-            symbol1 == "4"-> symbol1 = "♡"
-            symbol1 == "5"-> symbol1 = "♢"
-            symbol1 == "6"-> symbol1 = "♤"
-            symbol1 == "7"-> symbol1 = "♧ "
-
+        choice1 = readlnOrNull()?.toIntOrNull()
+        if (choice1 !in 1..7) {
+            println("PLEASE CHOOSE NUMBER FROM 1 TO 7!!")
+            continue
         }
-    println("Player 1: $name1 $symbol1 ")
-    player1Symbol = symbol1
 
-    // ger user 2 name
+        symbol1 = when (choice1) {
+            1 -> "♔"
+            2 -> "♕"
+            3 -> "⚐"
+            4 -> "♡"
+            5 -> "♢"
+            6 -> "♤"
+            7 -> "♧"
+            else -> ""
+        }
+        break
+    }
+        println("Player 1: $name1 $symbol1 ")
+        player1Symbol = symbol1
+    }
+
+fun getPlayer2Name() {
+    // get user 2 name
     print(" Enter player 2 name here : ")
     val name2 = readln()
-
+    var choice2: Int?
+    val symbol2: String
     // Get user 2 symbol
+    while (true) {
     println("1.♚")
     println("2.♛")
     println("3.⚑")
@@ -81,25 +100,31 @@ fun getPlayerName(){
     println("6.♠")
     println("7.♣")
     println("Choose player 2 symbol to play: ")
-
-    var symbol2 = readln()
-    when {
-        symbol2 == "1"-> symbol2 = "♚"
-        symbol2 == "2"-> symbol2 = "♛"
-        symbol2 == "3"-> symbol2 = "⚑"
-        symbol2 == "4"-> symbol2 = "♥"
-        symbol2 == "5"-> symbol2 = "♦"
-        symbol2 == "6"-> symbol2 = "♠"
-        symbol2 == "7"-> symbol2 = "♣"
+    choice2 = readlnOrNull()?.toIntOrNull()
+    if (choice2 !in 1..7) {
+        println("PLEASE CHOOSE NUMBER FROM 1 TO 7!!")
+        continue
     }
-
-    println("Player 2: $name2 $symbol2")
-    player2Symbol = symbol2
+    symbol2 = when (choice2) {
+        1 -> "♚"
+        2 -> "♛"
+        3 -> "⚑"
+        4 -> "♥"
+        5 -> "♦"
+        6 -> "♠"
+        7 -> "♣"
+        else -> ""
+    }
+    break
 }
+            println("Player 2: $name2 $symbol2")
+            player2Symbol = symbol2
+        }
+
 // Create the  board
 fun createBoard() {
     repeat(12) {
-           boards.add("SPACE ")
+           boards.add("")
    }
 }
 // Show the board
@@ -126,19 +151,48 @@ fun showBoard(){
 }
 
 fun getUser1Movement() {
-    var movement: String
     while (true) {
-        print("Choose number player 1 move: ")
-        movement = readln()
-        if (movement.isNotBlank()) break
+        var position: Int?
+        while (true) {
+            print("Player 1, where to place your counter: ")
+            position = readlnOrNull()?.toIntOrNull()
+            if (position != null && position in 1..12) break
+        }
 
+        val index = position!! - 1
+
+
+        if (boards[index] == "") {
+            boards[index] = player1Symbol
+            break
+        } else {
+            println("THIS SPACE HAS BEEN USED!!!")
+        }
     }
-        var movementAdded = player1Symbol
+}
+fun getUser2Movement() {
+    while (true) {
+        var position: Int?
+        while (true) {
+            print("Player 2, where to place your counter: ")
+            position = readlnOrNull()?.toIntOrNull()
+            if (position != null && position in 1..12) break
+        }
 
+        val index = position!! - 1
+
+
+        if (boards[index] == "") {
+            boards[index] = player2Symbol
+            break
+        } else {
+            println("THIS SPACE HAS BEEN USED!!!")
+        }
     }
+}
+fun explodeChain(){
 
-
-
+}
 
 
 
